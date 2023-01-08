921fb414342026d7b2949d0308b2320c5e9a8212
aashnasaxena <120560555+aashnasaxena@users.noreply.github.com>
2023-01-08 20:07:54 +0530

    Update PCA.py

--- Code/PCA.py
+++ Code/PCA.py
@@ -4,9 +4,12 @@ import os
 import glob 
 from multiprocessing import Pool
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans 
 import math 
 
 cwd = os.getcwd()
+
+tlog = open("PCA12_teams.txt","w")
 
 tpdir = "/home/csb/Aashna/Range10/TOPO"
 
@@ -103,6 +106,44 @@ def theta(sol_path,t):
 	df3.sort_values(by = "Node", inplace = True)
 
 	return df3
+def theta12(sol_path,t):
+	df = pd.read_csv(sol_path)
+	df_sol = df.iloc[:,3:]
+	df_sol.drop(columns = ['CDH1','KLF8','TCF3','VIM','miR205','miR30c','miR9'], inplace = True)
+	genes = df_sol.columns.to_list()
+	pca = PCA(n_components = 2)
+	df_pca = pca.fit_transform(df_sol)
+	n = pca.n_components_
+	feat = pca.components_ 
+	e_val = pd.Series(np.sqrt(pca.explained_variance_))
+	df2 = pd.DataFrame(data = feat, index = np.arange(n), columns = genes)
+
+	for row in df2.index:
+		df2.iloc[row,:] = df2.iloc[row,:]*e_val[row]
+
+	df2['Basis'] = 0
+	df2.iloc[0,15] = 1
+	basis = df2.iloc[:,15]
+
+	d1 = basis 
+	vector = []
+	angles = []
+
+	for j in range(15):
+		v = df2.iloc[:,j]
+		vector.append(v)
+
+		for k in range(len(vector)):
+			angle = (np.arccos((np.dot(vector[k],d1))/((np.linalg.norm(vector[k]))*(np.linalg.norm(d1)))))*(180.0/math.pi)
+
+		angles.append(angle)
+
+	df3 = pd.DataFrame(data = None, index = np.arange(15), columns = ['Node',t[:-5]])
+	df3['Node'] = genes
+	df3[t[:-5]] = angles
+	df3.sort_values(by = "Node", inplace = True)
+
+	return df3
 
 pool = Pool(70)
 
@@ -111,6 +152,7 @@ PC2v = pool.map(pc2var,[(glob.glob(cwd + "/Results/" + t[:-5] + "/" + str(n) + "
 PCnum = pool.map(pcncomp,[(glob.glob(cwd + "/Results/" + t[:-5] + "/" + str(n) + "/*norm.csv")[0]) for n in range(1,4) for t in tpfl])
 PCcoeff = pool.starmap(cumulative,[(glob.glob(cwd + "/Results/" + t[:-5] + "/" + str(n) + "/*norm.csv")[0],t) for t in tpfl for n in range(1,4)])
 PCtheta = pool.starmap(theta,[(glob.glob(cwd + "/Results/" + t[:-5] + "/" + str(n) + "/*norm.csv")[0],t) for t in tpfl for n in range(1,4)])
+PCtheta12 = pool.starmap(theta,[(glob.glob(cwd + "/Results/" + t[:-5] + "/" + str(n) + "/*norm.csv")[0],t) for t in tpfl for n in range(1,4)])
 
 pool.close()
 pool.join()
@@ -173,36 +215,62 @@ for a in Afin:
 PCangle = pd.concat(Aframes, axis = 1)
 PCangle.sort_index(axis = 1, inplace = True)
 
+Afin12 = []
+for a in np.arange(0,(3*len(tpfl)),3):
+	ang = pd.concat(PCtheta12[a:a+3], axis = 0)
+	Afin.append(ang)
+Aframes12 = []
+j = 1
+for a in Afin12:
+	if j == 1:
+		Aframes12.append(a)
+	else:
+		Aframes12.append(a.iloc[:,1])
+	j += 1
+
+PCangle12 = pd.concat(Aframes12, axis = 1)
+PCangle12.sort_index(axis = 1, inplace = True)
+
 var1.to_csv(cwd + "/PC1variance.csv", index = False)
 var2.to_csv(cwd + "/PC2variance.csv", index = False)
 nums.to_csv(cwd + "/Num_PCs.csv", index = False)
 coeff.to_csv(cwd + "/PC_coefficients.csv", index = False)
 PCangle.to_csv(cwd + "/Theta.csv", index = False)
+PCangle12.to_csv(cwd + "/Theta12.csv", index = False)
 
-def PCA_teams(coeff_path):
-	df = pd.read_csv(coeff_path)
+def PCA_teams(theta):
+	df = pd.read_csv(theta)
 	#df.drop(columns = "Unnamed: 0", inplace = True)
 	df.sort_index(axis = 1, inplace = True)
 	cols = df.columns[:-1].to_list()
+	THETA = pd.DataFrame(data = None, index = np.arange(15), columns = np.arange(len(cols)))
+	for k in range(len(cols)):
+		THETA[k] = (theta.iloc[0:15,k]+theta.iloc[15:30,k]+theta.iloc[30:45,k])/3.0
+	THETA.columns = cols
+	THETA['Node'] = df.loc[:15,'Node'].to_list()
 	TS = []
 	for col in cols:
-		df.sort_values(by = [col], inplace = True)
-		for row in df.index:
-			if df.loc[row,col] >= 0:
-				t = row
-				break
-		team1 = df.loc[:t,col]
-		team1.drop(index = t, inplace = True)
-		team2 = df.loc[t:,col]
+		kmeans = KMeans(n_clusters = 2).fit(THETA[col].to_numpy().reshape(-1,1))
+		cluster = kmeans.predict(THETA[col].to_numpy().reshape(-1,1))
+		t1 = THETA[col][cluster == 0]
+		T1 = []
+		for i in t1.index:
+			T1.append(THETA.loc[i,'Node'])
+		t2 = THETA[col][cluster == 1]
+		T2 = []
+		for i in t2.index:
+			T2.append(THETA.loc[i,'Node'])
+		team1 = ""
+		team2 = ""
+		for g in T1:
+			team1 = team1 + "," + g
+		team1 = team1.replace(",","",1)
+		for g in T2:
+			team2 = team2 + "," + g
+		team2 = team2.replace(",","",1)
+		tlog.write(col + " t1 " + str(len(T1)) + " " + team1 + "\n" + col + " t2 " + str(len(T2)) + " " + team2 + "\n\n")
 
-		t1 = abs(team1.sum())
-		t2 = abs(team2.sum())
-		t_av = (t1+t2)/2.0
-		TS.append(t_av)
-
-	PCT = pd.DataFrame(data = None, index = np.arange(len(cols)), columns = ['Network', 'PCA_ts'])
-	PCT['Network'] = cols 
-	PCT['PCA_ts'] = TS
-	PCT.to_csv(cwd + "/PCA_ts.csv", index = False)
-
+PCA_teams(cwd + "/Theta12.csv")
+		
+	
 
