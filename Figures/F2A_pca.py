15d6c3ffbd2f95d4f61aa9ca549e00a2e11c844c
aashnasaxena <120560555+aashnasaxena@users.noreply.github.com>
2023-01-17 23:00:58 +0530

    Update F2A_pca.py

--- Figures/F2A_pca.py
+++ Figures/F2A_pca.py
@@ -29,7 +29,7 @@ sns.set(rc={"figure.figsize":(10.5,8)})
 sns.set_style("ticks",rc={"axes.facecolor":"white","axes.edgecolor":"black","font.family":'sans-serif',"font.sans-serif":'Arial',"font.size":18})
 sns.set_context({"font.weight":"normal","font.size":18,"font.style":"normal","axes.labelsize":18,"axes.labelpad":8,"axes.labelweight":"bold","xtick.labelsize":18,"ytick.labelsize":18,"legend.edgecolor":"white","legend.facecolor":"white","legend.title_fontsize":18,"legend.fontsize":18})
 
-sns.scatterplot(data = final, x = "PC1", y = "PC2", hue = "Team", palette = ["#a50f15","#3690c0"], s = 3)
+sns.scatterplot(data = final, x = "PC1", y = "PC2", hue = "Team", palette = ['#509e90', '#1d6c8a'], s = 3)
 #palette = "crest"
 plt.legend(bbox_to_anchor=(1.02,1), loc = 2, borderaxespad = 0)
 plt.tight_layout()
