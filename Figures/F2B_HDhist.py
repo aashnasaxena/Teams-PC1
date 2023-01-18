127cdadfc1bd7cb8923523d7023362ef60651a3f
aashnasaxena <120560555+aashnasaxena@users.noreply.github.com>
2023-01-18 14:12:28 +0530

    Update F2B_HDhist.py

--- Figures/F2B_HDhist.py
+++ Figures/F2B_HDhist.py
@@ -19,7 +19,7 @@ sns.set(rc={"figure.figsize":(10.5,8)})
 sns.set_style("ticks",rc={"axes.facecolor":"white","axes.edgecolor":"black","font.family":'sans-serif',"font.sans-serif":'Arial',"font.size":18})
 sns.set_context({"font.weight":"normal","font.size":18,"font.style":"normal","axes.labelsize":18,"axes.labelpad":8,"axes.labelweight":"bold","xtick.labelsize":18,"ytick.labelsize":18,"legend.edgecolor":"white","legend.facecolor":"white","legend.title_fontsize":18,"legend.fontsize":18})
 
-sns.histplot(data = final, x = "PC1 Variance", stat = 'probability', bins = 'auto', element = 'step')
+sns.histplot(data = final, x = "Hamming Distance", stat = 'probability', bins = 'auto', element = 'step')
 plt.axvline(bio, color = '#bd0026', linewidth = 3)
 plt.tight_layout()
 
