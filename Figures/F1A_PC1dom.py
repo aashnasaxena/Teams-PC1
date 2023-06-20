be84610b532b837661a3641dc061c17dd4784234
aashnasaxena <120560555+aashnasaxena@users.noreply.github.com>
2023-06-20 10:38:25 +0530

    Update F1A_PC1dom.py

--- Figures/F1A_PC1dom.py
+++ Figures/F1A_PC1dom.py
@@ -17,7 +17,4 @@ plt.axvline(bio, color = '#bd0026', linewidth = 3)
 plt.xlim(0,1)
 plt.savefig("F1A_PC1dom.png", dpi = 400, pad_inches = 0)
 plt.savefig("F1A_PC1dom.svg", dpi = 400)
-
-sns.set(rc={"figure.figsize":(10.5,8)})
-sns.set_style("ticks",rc={"axes.facecolor":"white","axes.edgecolor":"black","font.family":'sans-serif',"font.sans-serif":'Arial',"font.size":18})
-sns.set_context({"font.weight":"normal","font.size":18,"font.style":"normal","axes.labelsize":18,"axes.labelpad":8,"axes.labelweight":"bold","xtick.labelsize":18,"ytick.labelsize":18,"legend.edgecolor":"white","legend.facecolor":"white","legend.title_fontsize":18,"legend.fontsize":18})
+plt.clf()
